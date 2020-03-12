ca9c1e1b21824b7116a21a758a0c937ad5fbbe2d
PeggyBrown <ola.qnysz@gmail.com>
2020-03-12 20:35:17 +0100

    and fix the code (Python 3.7.3)

--- binary_search.py
+++ binary_search.py
@@ -1,8 +1,8 @@
-def binary_search(list, item)
+def binary_search(list, item):
     low = 0
-    hight = len(list) - 1
-    while low <= hight:
-        mid = (low + hight) / 2
+    high = len(list) - 1
+    while low <= high:
+        mid = (low + high) // 2
         guess = list[mid]
         if guess == item:
             return mid
@@ -10,8 +10,8 @@ def binary_search(list, item)
             high = mid - 1
         else:
             low = mid + 1
-        return None
+    return None
 
 my_list = [1, 2, 5, 7, 9]
-Print binary_search(my_list, 3)
-Print binary_search(my_list, -1) 
+print(binary_search(my_list, 2))
+print(binary_search(my_list, -1))
